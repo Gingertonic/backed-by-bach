6fe2d5ce37c45022f482432a5cbb9d690d3dd50c
Gingertonic <bethmschofield@gmail.com>
2019-08-14 20:44:15 +0100

    setup webaudio api

--- daw.js
+++ daw.js
@@ -1,1 +1,31 @@
-console.log("loaded daw")
+// Create an audio context (allow for legacy browsers)
+let audioContext
+let volume
+
+// Existing code unchanged.
+window.onload = function() {
+  document.getElementById('bob').addEventListener('click', function() {
+    const AudioContext = window.AudioContext || window.webkitAudioContext;
+    audioContext = new AudioContext();
+   
+    volume = audioContext.createGain()
+    volume.connect(audioContext.destination)
+    volume.gain.value = 0.2
+
+    createChord()
+  });
+}
+
+
+const createChord = () => {
+    notes = [440, 523.25, 698.46]
+    notes.forEach(createNote)
+  }
+
+ const createNote = f => {
+    var sine = audioContext.createOscillator();
+    sine.frequency.value = f
+    sine.type = "sine"
+    sine.start();
+    sine.connect(volume);
+  } 
