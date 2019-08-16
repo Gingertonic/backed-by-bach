62247dbb75e7c47d19b16b2f77012cb88b672793
Gingertonic <bethmschofield@gmail.com>
2019-08-16 01:25:52 +0100

    implement basic points flow

--- daw.js
+++ daw.js
@@ -1,4 +1,3 @@
-// Create an audio context (allow for legacy browsers)
 let audioContext
 let masterVolume
 let playback
@@ -46,36 +45,39 @@ let chords = {
 }
 
 let state = {
-    current: null
+    current: null,
+    points: 0
 }
 
 window.onload = function() {
   document.getElementById('toggle').addEventListener('click', setupStudio);
   document.getElementById('play').addEventListener('click', startPlayback);
+  document.getElementById('submit').addEventListener('click', getBach);
   document.querySelectorAll('.chord-slider .slider').forEach(slider => slider.addEventListener('change', updateNote))
   document.querySelector('#master').addEventListener('change', updateMasterVolume)
 }
 
 const createChords = () => {
     for (let ch in chords){createNotes(ch)}
-  }
+}
 
-  const createNotes = (chord) => {
-      chords[chord].notes.forEach(n => createNote(n, chord))
-  }
+const createNotes = (chord) => {
+    chords[chord].notes.forEach(n => createNote(n, chord))
+}
 
- const createNote = (n, chord) => {
+const createNote = (n, chord) => {
     var sine = audioContext.createOscillator();
     sine.frequency.value = n.f 
     sine.type = "sine"
     sine.start();
     sine.connect(chords[chord].volChannel);
     n.ch = sine
-  } 
+} 
 
 const updateNote = e => {
     let chord = chords[e.currentTarget.parentNode.id]
     note = chord.notes.find(n => n.name == e.target.id)
+    note.f = parseInt(e.target.value)
     note.ch.frequency.value = parseInt(e.target.value)
 }
 
@@ -84,7 +86,7 @@ const setupStudio = () => {
     audioContext = new AudioContext();
    
     masterVolume = audioContext.createGain()
-    masterVolume.gain.value = 0.2
+    masterVolume.gain.value = 0
 
     for(let ch in chords){
         chords[ch].volChannel = audioContext.createGain()
@@ -97,6 +99,7 @@ const setupStudio = () => {
 
 const startPlayback = () => {
     playback = setInterval(playSequence, 1000)
+    masterVolume.gain.value = 0.1
     masterVolume.connect(audioContext.destination)
 }
 
@@ -109,6 +112,7 @@ const playSequence = () => {
         case "third":
             state.current = "fourth"; break;
         case "fourth":
+            masterVolume.gain.value = 0;
             masterVolume.disconnect();
             clearInterval(playback); state.current = null; break;
         default: state.current = "first"; break;
@@ -123,4 +127,20 @@ const playChord = ch => {
 
 const updateMasterVolume = e => {
     masterVolume.gain.value = (parseInt(e.target.value)/100)
+}
+
+const getBach = () => {
+    if (chords["first"].notes[0].f - chords["first"].notes[1].f < 0) {
+        state.points += 2
+        alert("Bach approves!")
+    } else {
+        state.points -= 1
+        alert("Bach does not approve...")
+    }
+    updatePointsDisplay()
+}
+
+const updatePointsDisplay = () => {
+    document.getElementById('points').innerHTML = state.points
+    state.points >= 10 ? alert("You have been officially Backed By Bach!") : null
 }
